102f52df75cb1b9e0abda2a763a547fc94e60f81
dbradham <davebradham@gmail.com>
2019-04-20 20:41:59 -0500

    add images

--- data/amenities.py
+++ data/amenities.py
@@ -15,7 +15,10 @@ for row in dataset.itertuples():
 	for a in am:
 		a = a.strip('"')
 		if a not in mem and 'ther' not in mem and '_49' not in a and 'Stair g' not in a and ' hoist' not in a and 'corner guar' not in a and a != '' and '_50' not in a:
+			a = a.replace(' ', '_')
 			mem.append(a)
+print(mem)
+print(len(mem))
 for i in range(len(mem)):
 	mem[i] = [mem[i], [0]]
 for row in dataset.itertuples():
@@ -48,7 +51,6 @@ hitlist = sorted(hitlist, reverse=True)
 for x in hitlist:
 	del mem[x]
 for m in mem:
-	print(len(m[1]))
 	for i in m[1]:
 		if i != 1 and i != 0:
 			print(i)
